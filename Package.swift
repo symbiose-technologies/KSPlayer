d8359c4c946cddf3c2efa520e5dd2d89d78867d7
Ryan McKinney <rydamckinney@gmail.com>
2023-03-28 16:05:59 -0400

    Build fixes

--- Package.swift
+++ Package.swift
@@ -13,6 +13,9 @@ let package = Package(
             targets: ["KSPlayer"]
         ),
     ],
+    dependencies: [
+        .package(url: "https://github.com/symbiose-technologies/FFmpegKit.git", branch: "symbiose")
+    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         .target(
@@ -36,22 +39,23 @@ let package = Package(
             resources: [.process("Resources")]
         ),
     ]
+    
 )
-var ffmpegKitPath = FileManager.default.currentDirectoryPath + "/FFmpegKit"
-if !FileManager.default.fileExists(atPath: ffmpegKitPath), let url = URL(string: #file) {
-    let path = url.deletingLastPathComponent().path
-    ffmpegKitPath = path + "/FFmpegKit"
-    if !FileManager.default.fileExists(atPath: ffmpegKitPath) {
-        ffmpegKitPath = path + "/../FFmpegKit"
-    }
-}
-
-if FileManager.default.fileExists(atPath: ffmpegKitPath) {
-    package.dependencies += [
-        .package(path: ffmpegKitPath),
-    ]
-} else {
-    package.dependencies += [
-        .package(url: "https://github.com/symbiose-technologies/FFmpegKit.git", branch: "symbiose"),
-    ]
-}
+//var ffmpegKitPath = FileManager.default.currentDirectoryPath + "/FFmpegKit"
+//if !FileManager.default.fileExists(atPath: ffmpegKitPath), let url = URL(string: #file) {
+//    let path = url.deletingLastPathComponent().path
+//    ffmpegKitPath = path + "/FFmpegKit"
+//    if !FileManager.default.fileExists(atPath: ffmpegKitPath) {
+//        ffmpegKitPath = path + "/../FFmpegKit"
+//    }
+//}
+//
+//if FileManager.default.fileExists(atPath: ffmpegKitPath) {
+//    package.dependencies += [
+//        .package(path: ffmpegKitPath),
+//    ]
+//} else {
+//    package.dependencies += [
+//        .package(url: "https://github.com/symbiose-technologies/FFmpegKit.git", branch: "symbiose"),
+//    ]
+//}
